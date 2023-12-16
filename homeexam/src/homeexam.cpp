8d2095dce4b4253b8d90c84f1fcc685a62ec2bd8
CdFerneding <bencf@stud.ntnu.no>
2023-12-16 10:02:26 +0100

    add the toggle texture state for task 3.

--- homeexam/src/homeexam.cpp
+++ homeexam/src/homeexam.cpp
@@ -58,11 +58,11 @@ void HomeExamApplication::key_callback(GLFWwindow* window, int key, int scancode
         case GLFW_KEY_ESCAPE:
             glfwSetWindowShouldClose(window, GLFW_TRUE);
             break;
-            // exit program
+        // exit program
         case GLFW_KEY_Q:
             getHomeExamApplication()->exit();
             break;
-            // handle camera changes
+        // handle camera changes
         case GLFW_KEY_S:
             getHomeExamApplication()->zoom(1.0f);
             break;
@@ -72,13 +72,12 @@ void HomeExamApplication::key_callback(GLFWwindow* window, int key, int scancode
         case GLFW_KEY_D:
             getHomeExamApplication()->rotate(5);
             break;
-        case GLFW_KEY_A:
+        case GLFW_KEY_A:    
             getHomeExamApplication()->rotate(-5);
             // counter-clockwise rotation
             break;
-            // handle unit selection
         case GLFW_KEY_T:
-            // getHomeExamApplication()->setTextureState();
+            getHomeExamApplication()->setTextureState();
             break;
         default:
             break;
